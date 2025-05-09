d931a74e5da04b12a93d00e7a31dda05dc22352c
Keita Ito <keitaito@keitaito.net>
2025-05-10 00:48:17 +0900

    chore: linter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 import "./globals.css";
-import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Multi-Layer Radial Picker",
